fe6387111b62fc661b9b234131f678f652cd7c1f
mmartins-vitor <mmartins.vitor@hotmail.com>
2023-07-20 22:01:29 -0300

    Example by documentation

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../chave.json")
+
+  project = "terraform-doc"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "firstvm" {
+  name         = "firstvm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "echo ola > /test.txt"
+
+}
